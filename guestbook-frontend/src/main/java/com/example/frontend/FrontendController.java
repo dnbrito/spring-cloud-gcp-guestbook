fd7452efe6eb3ecab9dd48e5896e7c632e0a3e90
“Diego <dnbrito.ifce@gmail.com>
2019-11-11 01:22:17 -0300

    Integrating Cloud Pub/Sub with Spring
    -> Teste o aplicativo no Cloud Shell
    
    1. No Cloud Shell, mude para o guestbook-servicediretório 
    cd ~/guestbook-service
    
    2. Execute o aplicativo de serviço de back-end. 
    ./mvnw -q spring-boot:run -Dserver.port=8081 -Dspring.profiles.active=cloud
    
    3. Mude para o guestbook-frontenddiretório 
    cd ~/guestbook-frontend
    
    4. Inicie o aplicativo frontend com o cloudperfil. 
    ./mvnw spring-boot:run -Dspring.profiles.active=cloud
    
    6. Abra a visualização da web do Cloud Shell e poste uma mensagem. 
    7. Abra uma nova guia de sessão do Cloud Shell e verifique a assinatura do Cloud Pub / Sub para obter as mensagens publicadas. 
    gcloud pubsub subscriptions pull messages-subscription-1 --auto-ack
    
    Nota 
    O Spring Integration for Cloud Pub / Sub funciona tanto para mensagens de entrada quanto para mensagens de saída. O Cloud Pub / Sub também suporta o Spring Cloud Stream para criar microsserviços reativos.

--- guestbook-frontend/src/main/java/com/example/frontend/FrontendController.java
+++ guestbook-frontend/src/main/java/com/example/frontend/FrontendController.java
@@ -11,12 +11,15 @@ import org.springframework.cloud.gcp.pubsub.core.*;
 @Controller
 @SessionAttributes("name")
 public class FrontendController {
-
+    
 	@Autowired
 	private GuestbookMessagesClient client;
 
 	@Autowired
 	private PubSubTemplate pubSubTemplate;
+
+	@Autowired
+	private OutboundGateway outboundGateway;
 	
 	@Value("${greeting:Hello}")
 	private String greeting;
@@ -35,12 +38,13 @@ public class FrontendController {
 	public String post(@RequestParam String name, @RequestParam String message, Model model) {
 		model.addAttribute("name", name);
 		if (message != null && !message.trim().isEmpty()) {
-            pubSubTemplate.publish("messages", name + ": " + message);
 			// Post the message to the backend service
 			Map<String, String> payload = new HashMap<>();
 			payload.put("name", name);
 			payload.put("message", message);
 			client.add(payload);
+
+			outboundGateway.publishMessage(name + ": " + message);
 		}
 		return "redirect:/";
   }
