ac3677c7263793b70b8561ce578b910c9c5ab69c
Diego Brito <dnbrito.ifce@gmail.com>
2019-11-11 20:13:29 -0300

    Working with Cloud Spanner
    -> Ative a API do Cloud Spanner
    
    1. Ative a API Cloud Spanner.
    
    gcloud services enable spanner.googleapis.com
    
    -> Crie e provisione uma instância do Cloud Spanner
    
    ie uma instância do Cloud Spanner.
    
    gcloud spanner instances create guestbook --config=regional-us-central1 \
      --nodes=1 --description="Guestbook messages"
    
    Crie um messagesbanco de dados na instância do Cloud Spanner.
    
    gcloud spanner databases create messages --instance=guestbook
    
    Confirme se o banco de dados existe, listando os bancos de dados da instância do Cloud Spanner.
    
    gcloud spanner databases list --instance=guestbook
    
    A saída indica que o banco de dados está pronto:
    
    NAME      STATE
    messages  READY
    Crie uma tabela no banco de dados Cloud Spanner
    Você cria uma tabela no messagesbanco de dados criando um arquivo que contém uma instrução DDL e executando o comando.
    
    Na guestbook-servicepasta, crie a dbpasta.
    
    cd ~/guestbook-service
    mkdir db
    
    No editor de código do Cloud Shell, selecione Arquivo> Atualizar .
    
    No editor de código do Shell de Nuvem, crie um arquivo nomeado spanner.ddlno ~/guestbook-service/db/diretório
    
    Adicione os seguintes comandos ao spanner.ddlarquivo:
    
    CREATE TABLE guestbook_message (
        id STRING(36) NOT NULL,
        name STRING(255) NOT NULL,
        image_uri STRING(255),
        message STRING(255)
    ) PRIMARY KEY (id);
    
    No Cloud Shell, use gcloudpara executar o comando DDL para criar a tabela.
    
    gcloud spanner databases ddl update messages \
      --instance=guestbook --ddl="$(<db/spanner.ddl)"
    
    Abra a guia Console do GCP para sua sessão de laboratório.
    No menu Navegação, abra Armazenamento > __ Chave.
    Clique no nome da instância de mensagens do Livro de Visitas para abri-la.
    Clique no nome do banco de dados __messages __ para abri-lo.
    Você deve ver a tabela guestbook_message se o spanner.ddlarquivo foi processado com sucesso.
    
    Clique na tabela guestbook_message para abri-la.
    O banco de dados é aberto, mostrando a guia de detalhes do esquema. O esquema deve corresponder ao esquema que você criou no spanner.ddlarquivo.
    
    Clique na guia dados .
    Ainda não há dados na tabela.
    
    No Cloud Shell, use gcloudpara executar o comando DDL para criar a tabela.
    
    gcloud spanner databases ddl update messages \
      --instance=guestbook --ddl="$(<db/spanner.ddl)"

--- guestbook-service/src/main/java/com/example/guestbook/GuestbookMessageRepository.java
+++ guestbook-service/src/main/java/com/example/guestbook/GuestbookMessageRepository.java
@@ -2,9 +2,11 @@ package com.example.guestbook;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
 @RepositoryRestResource
-public interface GuestbookMessageRepository extends 
-	PagingAndSortingRepository<GuestbookMessage, Long> {
+public interface GuestbookMessageRepository extends PagingAndSortingRepository<GuestbookMessage, String> {
+    
+    List<GuestbookMessage> findByName(String name);
 }
 
